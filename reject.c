a416d2e187d2f9d005e7a9ef3e2f6369627d80c1
artfin <artfin@mail.ru>
2025-03-02 17:13:49 +0300

    reject: worked out the estimate's error

--- reject.c
+++ reject.c
@@ -1,29 +1,36 @@
-// ????????
+// naive version of proposal:
 // P = 2 => rel.diff: 2.13% (60mln)
 // P = 2 => rel.diff: 1.29% (120mln)
 // P = 2 => rel.diff: 1.13% (240mln)
 // P = 2 => rel.diff: 1.06% (480mln)
 // 
-// P = 2 
-// UPDATED proposal, uniform margin: rel.diff: 0.80% (24mln)
-//                   normal  margin: 1.8885e-02, rel.diff: 0.9% (400mln)  
+// 300K: P = 2
+//   SIGMA = 0.20
+// acceptance rate: 89% 
+// UPDATED proposal, uniform margin (0.8): rel.diff: 0.80% (24mln)
+//                   normal  margin: 1.8885e-02, rel.diff: 0.90% (400mln)  
 //                                   1.8896e-02, rel.diff: 0.95% (1600mln)  
-//                                   1.8920e-02, rel.diff: 1.08% (6400mln) 
-/*
- * NOTES on the parameter values:
- *  for P = 4 at 300K
- *   SIGMA = 0.26
- *   MARGIN = 0.8
- *  for P = 2 at 300K
- *   SIGMA = 0.20
- *   MARGIN = 0.8
- */
+//                                   1.8920e-02, rel.diff: 1.08% (6400mln)
+//                                   1.8907e-02, rel.diff: 1.02% (25.6bln)
+//                                   1.8912e-02, rel.diff: 1.04% (128bln)
+// 300K: P = 4
+//   SIGMA = 0.26
+// acceptance rate: 1.03%
+// UPDATED proposal, normal margin:  1.8990e-02, rel.diff: 1.46% (2000mln)
+//                                   1.8970e-02, rel.diff: 1.35% (7040mln) 
+//
+// 300K: P = 8
+//   SIGMA = 0.29 -- still fails, trying out 0.30
+// acceptance rate: 3e-6%
+            
+// Pooled variance: https://en.wikipedia.org/wiki/Pooled_variance 
 
 #include <stdio.h>
 #include <math.h>
 #include <stdbool.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <inttypes.h>
 #include <mpi.h>
 
 #define HEAR_IMPLEMENTATION
@@ -42,15 +49,16 @@
 
 #define NBEADS 2
 
-#define SIGMA 0.20
-#define MARGIN 0.8
-#define RMIN 4.0
+#define SIGMA 0.27 // the width of normally distributed for 'dx' around zeroth bead
+#define MARGIN 0.8 // the width of uniform distribution for 'dx' around zeroth bead
+#define RMIN 4.0   
 #define XMAX 30.0
 #define VOLUME (2*XMAX)*(2*XMAX)*(2*XMAX)
 #define Temperature 300.0 
 #define beta 1.0/(Boltzmann_Hartree * Temperature)
-    
-double mu2_ref_value = 4.0/3.0*M_PI*30.0*30.0*30.0 * 1.655e-07; // mean(mu^2) for HeAr [300K]: int(mu^2 exp(-H/kT))/int(exp(-H/kT))
+
+
+#define REFVALUE (4.0/3.0*M_PI*30.0*30.0*30.0 * 1.655e-07) // mean(mu^2) for HeAr [300K]: int(mu^2 exp(-H/kT))/int(exp(-H/kT)) 
 //double mu2_ref_value = 4.0/3.0*M_PI*30.0*30.0*30.0 * 1.32216e-07; // mean(mu^2) for HeAr [10K]: int(mu^2 exp(-H/kT))/int(exp(-H/kT))
 
 
@@ -142,7 +150,10 @@ bool reject(mt_state* mts, double *x)
     return u < alpha;
 } 
 
-void make_estimate(mt_state *mts, size_t npoints, double *m_integral, double *q_integral)
+void make_estimate(mt_state *mts, size_t npoints, double *mean, double *var)
+// This function will return the mean and variance of sampled sequence.
+// The variance of mean is 'npoints' times smaller: sigma_mu^2 = sigma^2/N, 
+// this division is performed by caller of the function. 
 {
     int wrank, wsize;
     MPI_Comm_size(MPI_COMM_WORLD, &wsize);
@@ -153,6 +164,7 @@ void make_estimate(mt_state *mts, size_t npoints, double *m_integral, double *q_
     
     double *x = malloc(3*NBEADS * sizeof(double)); 
     
+    FILE *fd = fopen("samples.txt", "w");
     
     for (attempted = 0; accumulated < npoints; attempted++) {
         sample(mts, x);
@@ -164,27 +176,27 @@ void make_estimate(mt_state *mts, size_t npoints, double *m_integral, double *q_
                 fval = fval + VOLUME*dip_HeAr(r)*dip_HeAr(r)/NBEADS;
             }
 
-            double diff = fval - *m_integral;
-            *m_integral += diff / (accumulated + 1.0);
-            *q_integral += diff * diff * (accumulated / (accumulated + 1.0));
+            double diff = fval - *mean;
+            *mean += diff / (accumulated + 1.0);
+            *var += diff * diff * (accumulated / (accumulated + 1.0));
             accumulated++;
             
-            //fprintf(fd, "%.5e\n", x[3] - x[0]); 
+            // fprintf(fd, "%.5e %.5e %.5e %.5e\n", x[3] - x[0], x[6] - x[3], x[9] - x[6], x[12] - x[9]); 
 
-            //if ((accumulated > 0) && (accumulated % 1000000 == 0)) {
+            //if ((accumulated > 0) && (accumulated % 10 == 0)) {
             //    printf("[%d] Accumulated %zu/%zu, acceptance rate: %.2e%%\n", 
             //            wrank, accumulated, npoints, (float)accumulated/attempted*100.0);
             //    printf("[%d] Integral: %.6e +/- %.6e, reference value: %.10e, rel.diff: %.2f%%\n\n", 
-            //           wrank, VOLUME * *m_integral, VOLUME*sqrt(*q_integral / accumulated / (accumulated - 1.0)), 
-            //           mu2_ref_value, (VOLUME * *m_integral - mu2_ref_value)/mu2_ref_value*100.0);
-    
-            //}
+            //           wrank, VOLUME * *mean, VOLUME*sqrt(*var / accumulated / (accumulated - 1.0)), 
+            //           mu2_ref_value, (VOLUME * *mean - mu2_ref_value)/mu2_ref_value*100.0);
+            // }
         } 
     }
 
-    *q_integral = sqrt(*q_integral/accumulated/(accumulated-1.0));
+    *var = *var/(accumulated-1.0);
 
     free(x);
+    fclose(fd); 
 }
 
 int main(int argc, char *argv[])
@@ -196,64 +208,84 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &wrank);
     
     mt_state mts = {0};
-    int seeds[10] = {
-        5856844, 53218983, 91452731, 43166694, 49914920, 31614602
-    }; 
-    assert(wsize <= 10);
-    mts_seed32(&mts, seeds[wrank]);
-    printf("[%d] INFO: seed = %d\n", wrank, seeds[wrank]);
+    uint32_t seed = mts_goodseed(&mts);
+    printf("Initialized process (%d) with seed = %" PRIu32 "\n", wrank, seed); 
+    //int seeds[10] = {
+    //    5856844, 53218983, 91452731, 43166694, 49914920, 31614602
+    //}; 
+    //assert(wsize <= 10);
+    //mts_seed32(&mts, seeds[wrank]);
+    //printf("[%d] INFO: seed = %d\n", wrank, seeds[wrank]);
 
-    double tm_integral = 0.0; // total mean
-    double tq_integral = 0.0; // total stdev         
+    double total_mean = 0.0; 
+    double total_var = 0.0;
+    size_t total_samples_count = 0; // @todo: check that we don't overflow this at any point
     size_t packets = 0; 
 
-    // FILE *fd = fopen("samples.txt", "w");
     //size_t ACC_MAX = 400000000;
-    size_t iters = 160;
-    size_t npoints_per_iter = 10000000; 
+    size_t iters = 1000;
+    size_t packet_samples_count = 10000000; 
+           
+    if (wrank == 0) { 
+        printf("INFO: the requested calculation needs to collect = %zu, SIZE_MAX = %zu\n", 
+                iters*packet_samples_count*wsize, SIZE_MAX);
+        assert(iters*packet_samples_count*wsize < SIZE_MAX);
+    }
 
     for (size_t iter = 0; iter < iters; ++iter) {
-        double m_integral = 0.0; // mean
-        double q_integral = 0.0; // standard deviation
-        make_estimate(&mts, npoints_per_iter, &m_integral, &q_integral);
+        double packet_mean = 0.0; 
+        double packet_var = 0.0; 
+        make_estimate(&mts, packet_samples_count, &packet_mean, &packet_var);
 
         if (wrank == 0) {
-            tm_integral += m_integral;
-            tq_integral += q_integral;
+            double packet_stdev = sqrt(packet_var/packet_samples_count);
+            printf("[0] Adding packet (%zu/%zu) from %d = (%.5e +/- %.5e)\n", 
+                    packets, iters*wsize, 0, packet_mean, packet_stdev);
             packets++;
+
+            total_var = ((total_samples_count-1)*total_var + (packet_samples_count-1)*packet_var)/(total_samples_count-1 + packet_samples_count-1);
+            //total_var = ((total_samples_count-1)*total_var + (packet_samples_count-1)*packet_var) / 
+            //             (total_samples_count + packet_samples_count - 1) + 
+            //            total_samples_count*packet_samples_count*(total_mean - packet_mean)*(total_mean - packet_mean)/(total_samples_count + packet_samples_count) / 
+            //            (total_samples_count + packet_samples_count- 1);
+            total_mean = (total_mean*total_samples_count + packet_samples_count*packet_mean) / (total_samples_count + packet_samples_count);
+            total_samples_count += packet_samples_count;
 
             MPI_Status status;
             for (int i = 1; i < wsize; ++i) {
-                MPI_Recv(&m_integral, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, &status);
-                MPI_Recv(&q_integral, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, &status);
-                printf("[0] Received packet (%zu/%zu) from %d = (%.5e +/- %.5e)\n", 
-                        packets, iters*wsize, status.MPI_SOURCE, m_integral, q_integral);
+                MPI_Recv(&packet_mean, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, &status);
+                MPI_Recv(&packet_var, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, &status);
 
-                tm_integral += m_integral;
-                tq_integral += q_integral;
+                double packet_stdev = sqrt(packet_var/packet_samples_count);
+                printf("[0] Adding packet (%zu/%zu) from %d = (%.5e +/- %.5e)\n", 
+                        packets, iters*wsize, status.MPI_SOURCE, packet_mean, packet_stdev);
                 packets++;
+
+                total_var = ((total_samples_count-1)*total_var + (packet_samples_count-1)*packet_var)/(total_samples_count-1 + packet_samples_count-1);
+                // total_var = ((total_samples_count-1)*total_var + (packet_samples_count-1)*packet_var) / (total_samples_count + packet_samples_count - 1) + 
+                //  total_samples_count*packet_samples_count*(total_mean - packet_mean)*(total_mean - packet_mean)/(total_samples_count + packet_samples_count)/(total_samples_count + packet_samples_count- 1);
+                total_mean = (total_mean*total_samples_count + packet_samples_count*packet_mean) / (total_samples_count + packet_samples_count);
+                total_samples_count += packet_samples_count;
             }
 
-            printf("[0] Aggregate estimate %zu/%zu: %.5e +/- %.5e\n", 
-                    packets*npoints_per_iter, iters*wsize*npoints_per_iter,
-                    tm_integral/packets, tq_integral/packets);
+            double total_stdev = sqrt(total_var/total_samples_count);
+            printf("[0] Aggregate estimate %zu/%zu: %.6e +/- %.6e (%.2f%%)\n", 
+                    packets*packet_samples_count, iters*wsize*packet_samples_count,
+                    total_mean, total_stdev, total_stdev/total_mean*100.0);
         } else {
-            MPI_Send(&m_integral, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
-            MPI_Send(&q_integral, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+            MPI_Send(&packet_mean, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+            MPI_Send(&packet_var, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
         }
     } 
 
     if (wrank == 0) {
-        tm_integral /= packets;
-        tq_integral /= packets;
+        double total_stdev = sqrt(total_var);
         printf("---------------------------------------------------------------\n"); 
         //printf("FINAL: Accumulated %zu/%zu, acceptance rate: %.2e%%\n", accumulated, ACC_MAX, (float)accumulated/attempted*100.0);
-        printf("FINAL: Integral: %.6e +/- %.6e, reference value: %.10e, rel.diff: %.2f%%\n", 
-                tm_integral, sqrt(tq_integral / packets*npoints_per_iter / (packets*npoints_per_iter - 1.0)), 
-                mu2_ref_value, (tm_integral - mu2_ref_value)/mu2_ref_value*100.0);
+        printf("FINAL: Integral: %.6e +/- %.6e (%.2f%%), reference value: %.10e, rel.diff: %.2f%%\n", 
+                total_mean, total_stdev, total_stdev/total_mean*100.0, REFVALUE, (total_mean - REFVALUE)/REFVALUE*100.0);
     }
 
-    // fclose(fd); 
     MPI_Finalize();
 
     return 0;
