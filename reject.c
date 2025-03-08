ca33e7566ac2da0d33db80f9e249c7fcb33ca23a
artfin <artfin@mail.ru>
2025-03-08 21:43:33 +0300

    reject: testing MCMC for sampling difference

--- reject.c
+++ reject.c
@@ -1,8 +1,8 @@
 // naive version of proposal:
-// P = 2 => rel.diff: 2.13% (60mln)
-// P = 2 => rel.diff: 1.29% (120mln)
-// P = 2 => rel.diff: 1.13% (240mln)
-// P = 2 => rel.diff: 1.06% (480mln)
+//   P = 2 => rel.diff: 2.13% (60mln)
+//   P = 2 => rel.diff: 1.29% (120mln)
+//   P = 2 => rel.diff: 1.13% (240mln)
+//   P = 2 => rel.diff: 1.06% (480mln)
 // 
 // 300K: P = 2
 //   SIGMA = 0.20
@@ -12,15 +12,34 @@
 //                                   1.8896e-02, rel.diff: 0.95% (1600mln)  
 //                                   1.8920e-02, rel.diff: 1.08% (6400mln)
 //                                   1.8907e-02, rel.diff: 1.02% (25.6bln)
-//                                   1.8912e-02, rel.diff: 1.04% (128bln)
+//                                   1.8912e-02, rel.diff: 1.04% (128bln) -- 0.013% error
+// 100K: P = 2
+//   SIGMA = 0.35
+//                    normal margin: 7.6177e-03 (30bln) -- 0.02%
+//
+// 50 K: P = 2
+//   SIGMA = 0.50
+//                    normal margin: 5.3194e-03 (14bln) -- 0.02% 
+//    
+// -------------------------------------------------------------------------------------
 // 300K: P = 4
 //   SIGMA = 0.26
 // acceptance rate: 1.03%
 // UPDATED proposal, normal margin:  1.8990e-02, rel.diff: 1.46% (2000mln)
-//                                   1.8970e-02, rel.diff: 1.35% (7040mln) 
+//                                   1.8970e-02, rel.diff: 1.35% (7040mln) -- 0.05% error 
 //
+// 100K: P = 4
+//   SIGMA = 0.47
+//                   normal magin:   7.718137e-03 (1260mln) -- 0.1% error
+//                                   7.702722e-03 (4680mln) -- 0.05% error
+//
+// 50K: P = 4
+//   SIGMA = 0.65
+//                   normal magin:   5.4429e-03 (4500mln) -- 0.04% error
+//
+// -------------------------------------------------------------------------------------
 // 300K: P = 8
-//   SIGMA = 0.29 -- still fails, trying out 0.30
+//   SIGMA = 0.30 -- probably will work 
 // acceptance rate: 3e-6%
             
 // Pooled variance: https://en.wikipedia.org/wiki/Pooled_variance 
@@ -31,6 +50,7 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <inttypes.h>
+#include <string.h>
 #include <mpi.h>
 
 #define HEAR_IMPLEMENTATION
@@ -47,9 +67,22 @@
 
 #define Rmin 6.597835932201344e+00
 
-#define NBEADS 2
+#define NBEADS 4
 
-#define SIGMA 0.27 // the width of normally distributed for 'dx' around zeroth bead
+// Sampling scheme outline:
+// The construction of the proposal distribution is the following:
+//   first, we sample the zeroth bead with uniform distribution in the volume cube
+//   then we select sample the next P-1 beads with normal distribution with 
+//   variance set to SIGMA^2. 
+// Then, in the 'reject' function we compute the ratio of target distribution density
+//    exp(-sum_{i = 1}^{P}(m*P/(2*beta*hbar^2)*(x[i+1] - x[i])^2 + beta/P*V(x_i))
+// to proposal distribution density:
+//    exp(-(x[i]-x[0])^2/2*sigma^2) * exp(-beta*Vmin/P) 
+// In rejection scheme, this ratio gives us the probability to choose the current point as the point
+// of target distribution. If ratio turns out to be >= 1, it means that parameter SIGMA needs to
+// be increased.   
+
+#define SIGMA 0.25 // the width of normally distributed for 'dx' around zeroth bead
 #define MARGIN 0.8 // the width of uniform distribution for 'dx' around zeroth bead
 #define RMIN 4.0   
 #define XMAX 30.0
@@ -57,10 +90,19 @@
 #define Temperature 300.0 
 #define beta 1.0/(Boltzmann_Hartree * Temperature)
 
+#define MCMC_KSI_SIGMA 0.05
+#define MCMC_KSI_BURNIN 1000
+#define MCMC_KSI_SKIP 20
 
 #define REFVALUE (4.0/3.0*M_PI*30.0*30.0*30.0 * 1.655e-07) // mean(mu^2) for HeAr [300K]: int(mu^2 exp(-H/kT))/int(exp(-H/kT)) 
 //double mu2_ref_value = 4.0/3.0*M_PI*30.0*30.0*30.0 * 1.32216e-07; // mean(mu^2) for HeAr [10K]: int(mu^2 exp(-H/kT))/int(exp(-H/kT))
 
+typedef enum {
+    CALCULATION_M0,
+    CALCULATION_M1,
+} CALCULATION_TYPE;
+
+CALCULATION_TYPE ct = CALCULATION_M0;
 
 #define MT_GENERATE_CODE_IN_HEADER 0
 #include "mtwist.h"
@@ -99,6 +141,91 @@ void sample(mt_state* mts, double *x)
     }
 }
 
+
+double density(double *ksi) {
+    double C = mu*NBEADS/(2.0*beta); 
+    return exp(C * (-ksi[0]*ksi[0] - (ksi[2]-ksi[1])*(ksi[2]-ksi[1]) - ksi[2]*ksi[2] - (ksi[1]-ksi[0])*(ksi[1]-ksi[0])));
+}
+
+bool make_step(mt_state* mts, double *ksi1, double *ksi2, double *ksi3)
+{
+    double oldksi[NBEADS-1] = {*ksi1, *ksi2, *ksi3};
+    double ksi[NBEADS-1]    = {*ksi1, *ksi2, *ksi3};
+
+    for (size_t i = 0; i < NBEADS-1; ++i) {
+        ksi[i] = ksi[i] + generate_normal(mts, MCMC_KSI_SIGMA); 
+    }
+
+    double old_dens = density(oldksi);
+    double new_dens = density(ksi);
+
+    double alpha = new_dens/old_dens;
+    double u = mts_drand(mts);
+
+    if (u < alpha) {
+        // accept 
+        *ksi1 = ksi[0]; 
+        *ksi2 = ksi[1]; 
+        *ksi3 = ksi[2]; 
+        return true; 
+    }
+
+    return false;
+}
+
+void sample_with_mcmc(mt_state *mts, double *x, double *ksi, bool first)
+{
+    // sample new position of zeroth bead
+    // double xn[3]; 
+    // xn[0] = XMAX * (2.0*mts_drand(mts) - 1.0); // [-XMAX, XMAX]
+    // xn[1] = XMAX * (2.0*mts_drand(mts) - 1.0);
+    // xn[2] = XMAX * (2.0*mts_drand(mts) - 1.0);
+
+    // and move necklace to the new position of the zeroth bead without changing 
+    // distances between the beads 
+    // for (size_t i = 1; i < NBEADS; ++i) {
+    //     x[3*i + 0] += (xn[0] - x[0]); 
+    //     x[3*i + 1] += (xn[1] - x[1]); 
+    //     x[3*i + 2] += (xn[2] - x[2]); 
+    // }
+
+    // x[0] = xn[0];
+    // x[1] = xn[1];
+    // x[2] = xn[2];
+
+    // double ksi[3*(NBEADS - 1)] = {0};
+    // for (size_t i = 1; i < NBEADS; ++i) {
+    //     ksi[3*(i-1) + 0] = x[3*i + 0] - x[0]; 
+    //     ksi[3*(i-1) + 1] = x[3*i + 1] - x[1]; 
+    //     ksi[3*(i-1) + 2] = x[3*i + 2] - x[2]; 
+    // }
+
+    if (first) {
+        memset(ksi, 0, 3*(NBEADS-1)*sizeof(double));
+    
+        for (size_t i = 0; i < MCMC_KSI_BURNIN; ++i) {
+            make_step(mts, &ksi[0], &ksi[3], &ksi[6]);
+            make_step(mts, &ksi[1], &ksi[4], &ksi[7]);
+            make_step(mts, &ksi[2], &ksi[5], &ksi[8]);
+        } 
+    } else { 
+        x[0] = XMAX * (2.0*mts_drand(mts) - 1.0); // [-XMAX, XMAX]
+        x[1] = XMAX * (2.0*mts_drand(mts) - 1.0);
+        x[2] = XMAX * (2.0*mts_drand(mts) - 1.0);
+
+        for (size_t i = 0; i < MCMC_KSI_SKIP; ++i) {
+            make_step(mts, &ksi[0], &ksi[3], &ksi[6]);
+            make_step(mts, &ksi[1], &ksi[4], &ksi[7]);
+            make_step(mts, &ksi[2], &ksi[5], &ksi[8]);
+        }
+
+        for (size_t i = 1; i < NBEADS; ++i) {
+           x[3*i + 0] = ksi[3*(i-1) + 0] + x[0]; 
+           x[3*i + 1] = ksi[3*(i-1) + 1] + x[1]; 
+           x[3*i + 2] = ksi[3*(i-1) + 2] + x[2]; 
+        }
+    }
+}
 
 bool reject(mt_state* mts, double *x) 
 {
@@ -150,6 +277,37 @@ bool reject(mt_state* mts, double *x)
     return u < alpha;
 } 
 
+bool reject_with_mcmc(mt_state* mts, double *x) 
+{
+    double u = mts_drand(mts); // uniform [0, 1]
+   
+    double Vs = 0.0; 
+    for (size_t i = 0; i < NBEADS; ++i) {
+        double r = sqrt(x[3*i + 0]*x[3*i + 0] + x[3*i + 1]*x[3*i + 1] + x[3*i + 2]*x[3*i + 2]);
+        if (r < RMIN) return false;
+
+        Vs += beta*V_HeAr(r)/NBEADS;
+    }
+
+    double desired_proposal_ratio = exp(-Vs);  
+    
+    double Vmin = V_HeAr(Rmin); 
+    double M = exp(-beta*Vmin);
+    double alpha = desired_proposal_ratio/M;
+
+    if (alpha > 1.0) {
+        printf("x[0] = %.5e, x[1] = %.5e, x[2] = %.5e\n", x[0], x[1], x[2]);
+        printf("x[3] = %.5e, x[4] = %.5e, x[5] = %.5e\n", x[3], x[4], x[5]);
+        printf("desired_proposal_ratio = %.10e\n", desired_proposal_ratio);
+        printf("M = %.10e\n", M);
+
+        assert(false);
+    }
+
+    return u < alpha;
+} 
+
+
 void make_estimate(mt_state *mts, size_t npoints, double *mean, double *var)
 // This function will return the mean and variance of sampled sequence.
 // The variance of mean is 'npoints' times smaller: sigma_mu^2 = sigma^2/N, 
@@ -173,7 +331,19 @@ void make_estimate(mt_state *mts, size_t npoints, double *mean, double *var)
             double fval = 0.0;
             for (size_t i = 0; i < NBEADS; ++i) {
                 double r = sqrt(x[3*i + 0]*x[3*i + 0] + x[3*i + 1]*x[3*i + 1] + x[3*i + 2]*x[3*i + 2]);
-                fval = fval + VOLUME*dip_HeAr(r)*dip_HeAr(r)/NBEADS;
+
+                switch (ct) {
+                    case CALCULATION_M0: {
+                        fval = fval + VOLUME*dip_HeAr(r)*dip_HeAr(r)/NBEADS;
+                        break;
+                    }
+                    case CALCULATION_M1: {
+                        double dip = dip_HeAr(r);
+                        double dip_deriv = ddip_HeAr(r);
+                        fval = fval + VOLUME*(dip_deriv*dip_deriv + 2.0/r/r*dip*dip)/NBEADS; 
+                        break;
+                    }
+                } 
             }
 
             double diff = fval - *mean;
@@ -199,6 +369,196 @@ void make_estimate(mt_state *mts, size_t npoints, double *mean, double *var)
     fclose(fd); 
 }
 
+void make_estimate_with_mcmc_proposal(mt_state *mts, size_t npoints, double *mean, double *var)
+// This function will return the mean and variance of sampled sequence.
+// The variance of mean is 'npoints' times smaller: sigma_mu^2 = sigma^2/N, 
+// this division is performed by caller of the function. 
+{
+    int wrank, wsize;
+    MPI_Comm_size(MPI_COMM_WORLD, &wsize);
+    MPI_Comm_rank(MPI_COMM_WORLD, &wrank);
+
+    size_t accumulated = 0;
+    size_t attempted = 0; 
+    
+    FILE *fd = fopen("samples.txt", "w");
+
+    double x[3*NBEADS]; 
+    double ksi[3*(NBEADS-1)]; 
+    sample_with_mcmc(mts, x, ksi, true);
+    
+    for (attempted = 0; accumulated < npoints; attempted++) {
+        sample_with_mcmc(mts, x, ksi, false);
+
+        if (reject_with_mcmc(mts, x)) {
+            double fval = 0.0;
+            for (size_t i = 0; i < NBEADS; ++i) {
+                double r = sqrt(x[3*i + 0]*x[3*i + 0] + x[3*i + 1]*x[3*i + 1] + x[3*i + 2]*x[3*i + 2]);
+
+                switch (ct) {
+                    case CALCULATION_M0: {
+                        fval = fval + VOLUME*dip_HeAr(r)*dip_HeAr(r)/NBEADS;
+                        break;
+                    }
+                    case CALCULATION_M1: {
+                        double dip = dip_HeAr(r);
+                        double dip_deriv = ddip_HeAr(r);
+                        fval = fval + VOLUME*(dip_deriv*dip_deriv + 2.0/r/r*dip*dip)/NBEADS; 
+                        break;
+                    }
+                } 
+            }
+
+            double diff = fval - *mean;
+            *mean += diff / (accumulated + 1.0);
+            *var += diff * diff * (accumulated / (accumulated + 1.0));
+            accumulated++;
+            
+            //fprintf(fd, "%.5e %.5e %.5e\n", x[3] - x[0], x[6] - x[3], x[9] - x[6]); 
+
+            if ((accumulated > 0) && (accumulated % 100000 == 0)) {
+                printf("[%d] Accumulated %zu/%zu, acceptance rate: %.2e%%\n", 
+                        wrank, accumulated, npoints, (float)accumulated/attempted*100.0);
+            //    printf("[%d] Integral: %.6e +/- %.6e, reference value: %.10e, rel.diff: %.2f%%\n\n", 
+            //           wrank, VOLUME * *mean, VOLUME*sqrt(*var / accumulated / (accumulated - 1.0)), 
+            //           mu2_ref_value, (VOLUME * *mean - mu2_ref_value)/mu2_ref_value*100.0);
+            }
+        } 
+    }
+
+    *var = *var/(accumulated-1.0);
+
+    fclose(fd); 
+}
+
+/*
+bool make_mcmc_move_for_deltas(mt_state* mts, double *x)
+{
+    double oldx[3*NBEADS] = {0};
+    for (size_t i = 1; i < NBEADS; ++i) {
+        oldx[3*i + 0] = x[3*i + 0];
+        oldx[3*i + 1] = x[3*i + 1];
+        oldx[3*i + 2] = x[3*i + 2];
+    }
+    
+    double Vs = 0.0; 
+    for (size_t i = 0; i < NBEADS; ++i) {
+        double r = sqrt(x[3*i + 0]*x[3*i + 0] + x[3*i + 1]*x[3*i + 1] + x[3*i + 2]*x[3*i + 2]);
+        Vs += beta*V_HeAr(r)/NBEADS;
+    }
+
+    double C = mu*NBEADS/(2.0*beta);
+    double kin = 0.0;
+    for (size_t i = 1; i < NBEADS; ++i) {
+        kin = kin + C*((x[3*i + 0] - x[3*(i-1) + 0])*(x[3*i + 0] - x[3*(i-1) + 0]) + \
+                       (x[3*i + 1] - x[3*(i-1) + 1])*(x[3*i + 1] - x[3*(i-1) + 1]) + \
+                       (x[3*i + 2] - x[3*(i-1) + 2])*(x[3*i + 2] - x[3*(i-1) + 2]));
+    }
+    kin = kin + C*((x[0] - x[3*(NBEADS-1) + 0])*(x[0] - x[3*(NBEADS-1) + 0]) + \
+                   (x[1] - x[3*(NBEADS-1) + 1])*(x[1] - x[3*(NBEADS-1) + 1]) + \
+                   (x[2] - x[3*(NBEADS-1) + 2])*(x[2] - x[3*(NBEADS-1) + 2]));
+
+
+    double old_density = exp(-(kin + Vs));  
+    printf("kin = %.5e, Vs = %.5e => old_density = %.5e\n", kin, Vs, old_density);
+
+    for (size_t i = 1; i < NBEADS; ++i) {
+        x[3*i + 0] = x[3*i + 0] + generate_normal(mts, MCMC_SIGMA); 
+        x[3*i + 1] = x[3*i + 1] + generate_normal(mts, MCMC_SIGMA); 
+        x[3*i + 2] = x[3*i + 2] + generate_normal(mts, MCMC_SIGMA); 
+    }
+
+    Vs = 0.0; 
+    for (size_t i = 0; i < NBEADS; ++i) {
+        double r = sqrt(x[3*i + 0]*x[3*i + 0] + x[3*i + 1]*x[3*i + 1] + x[3*i + 2]*x[3*i + 2]);
+        Vs += beta*V_HeAr(r)/NBEADS;
+    }
+    
+    kin = 0.0;
+    for (size_t i = 1; i < NBEADS; ++i) {
+        kin = kin + C*((x[3*i + 0] - x[3*(i-1) + 0])*(x[3*i + 0] - x[3*(i-1) + 0]) + \
+                       (x[3*i + 1] - x[3*(i-1) + 1])*(x[3*i + 1] - x[3*(i-1) + 1]) + \
+                       (x[3*i + 2] - x[3*(i-1) + 2])*(x[3*i + 2] - x[3*(i-1) + 2]));
+    }
+    kin = kin + C*((x[0] - x[3*(NBEADS-1) + 0])*(x[0] - x[3*(NBEADS-1) + 0]) + \
+                   (x[1] - x[3*(NBEADS-1) + 1])*(x[1] - x[3*(NBEADS-1) + 1]) + \
+                   (x[2] - x[3*(NBEADS-1) + 2])*(x[2] - x[3*(NBEADS-1) + 2]));
+    
+    double new_density = exp(-(kin + Vs));  
+    printf("kin = %.5e, Vs = %.5e => new_density = %.5e\n", kin, Vs, new_density);
+
+    double u = mt_drand();
+    double alpha = exp(-(new_density - old_density));
+    printf("alpha = %.5e\n", alpha);
+
+    if (u > alpha) {
+        for (size_t i = 1; i < NBEADS; ++i) {
+            x[3*i + 0] = oldx[3*i + 0];
+            x[3*i + 1] = oldx[3*i + 1];
+            x[3*i + 2] = oldx[3*i + 2];
+        }
+
+        return false;
+    }
+   
+    return true; 
+}
+*/
+
+/*
+void make_mcmc_estimate(mt_state *mts, size_t npoints, double *mean, double *var)
+{
+    size_t accumulated = 0;
+    size_t attempted = 0; 
+    
+    double x[3*NBEADS]; 
+    sample(mts, x);
+    for (size_t i = 0; i < 1000; ++i) {
+        accumulated += make_mcmc_move_for_deltas(mts, x);
+    }
+    printf("Acceptance rate: %.3e%%\n", 100.0 * accumulated/1000);
+    
+    for (attempted = 0; accumulated < npoints; attempted++) {
+        if (make_mcmc_move_for_deltas(mts, x) && (attempted % 20 == 0)) {
+            double fval = 0.0;
+            for (size_t i = 0; i < NBEADS; ++i) {
+                double r = sqrt(x[3*i + 0]*x[3*i + 0] + x[3*i + 1]*x[3*i + 1] + x[3*i + 2]*x[3*i + 2]);
+
+                switch (ct) {
+                    case CALCULATION_M0: {
+                        fval = fval + VOLUME*dip_HeAr(r)*dip_HeAr(r)/NBEADS;
+                        break;
+                    }
+                    case CALCULATION_M1: {
+                        double dip = dip_HeAr(r);
+                        double dip_deriv = ddip_HeAr(r);
+                        fval = fval + VOLUME*(dip_deriv*dip_deriv + 2.0/r/r*dip*dip)/NBEADS; 
+                        break;
+                    }
+                } 
+            }
+
+            double diff = fval - *mean;
+            *mean += diff / (accumulated + 1.0);
+            *var += diff * diff * (accumulated / (accumulated + 1.0));
+            accumulated++;
+            
+            // fprintf(fd, "%.5e %.5e %.5e %.5e\n", x[3] - x[0], x[6] - x[3], x[9] - x[6], x[12] - x[9]); 
+
+            //if ((accumulated > 0) && (accumulated % 10 == 0)) {
+            //    printf("[%d] Accumulated %zu/%zu, acceptance rate: %.2e%%\n", 
+            //            wrank, accumulated, npoints, (float)accumulated/attempted*100.0);
+            //    printf("[%d] Integral: %.6e +/- %.6e, reference value: %.10e, rel.diff: %.2f%%\n\n", 
+            //           wrank, VOLUME * *mean, VOLUME*sqrt(*var / accumulated / (accumulated - 1.0)), 
+            //           mu2_ref_value, (VOLUME * *mean - mu2_ref_value)/mu2_ref_value*100.0);
+            // }
+        } 
+    }
+
+    *var = *var/(accumulated-1.0);
+}
+*/
+
 int main(int argc, char *argv[])
 {
     MPI_Init(&argc, &argv);
@@ -223,8 +583,8 @@ int main(int argc, char *argv[])
     size_t packets = 0; 
 
     //size_t ACC_MAX = 400000000;
-    size_t iters = 1000;
-    size_t packet_samples_count = 10000000; 
+    size_t iters = 100;
+    size_t packet_samples_count = 1000000; 
            
     if (wrank == 0) { 
         printf("INFO: the requested calculation needs to collect = %zu, SIZE_MAX = %zu\n", 
@@ -235,7 +595,7 @@ int main(int argc, char *argv[])
     for (size_t iter = 0; iter < iters; ++iter) {
         double packet_mean = 0.0; 
         double packet_var = 0.0; 
-        make_estimate(&mts, packet_samples_count, &packet_mean, &packet_var);
+        make_estimate_with_mcmc_proposal(&mts, packet_samples_count, &packet_mean, &packet_var);
 
         if (wrank == 0) {
             double packet_stdev = sqrt(packet_var/packet_samples_count);
@@ -244,10 +604,6 @@ int main(int argc, char *argv[])
             packets++;
 
             total_var = ((total_samples_count-1)*total_var + (packet_samples_count-1)*packet_var)/(total_samples_count-1 + packet_samples_count-1);
-            //total_var = ((total_samples_count-1)*total_var + (packet_samples_count-1)*packet_var) / 
-            //             (total_samples_count + packet_samples_count - 1) + 
-            //            total_samples_count*packet_samples_count*(total_mean - packet_mean)*(total_mean - packet_mean)/(total_samples_count + packet_samples_count) / 
-            //            (total_samples_count + packet_samples_count- 1);
             total_mean = (total_mean*total_samples_count + packet_samples_count*packet_mean) / (total_samples_count + packet_samples_count);
             total_samples_count += packet_samples_count;
 
@@ -262,8 +618,6 @@ int main(int argc, char *argv[])
                 packets++;
 
                 total_var = ((total_samples_count-1)*total_var + (packet_samples_count-1)*packet_var)/(total_samples_count-1 + packet_samples_count-1);
-                // total_var = ((total_samples_count-1)*total_var + (packet_samples_count-1)*packet_var) / (total_samples_count + packet_samples_count - 1) + 
-                //  total_samples_count*packet_samples_count*(total_mean - packet_mean)*(total_mean - packet_mean)/(total_samples_count + packet_samples_count)/(total_samples_count + packet_samples_count- 1);
                 total_mean = (total_mean*total_samples_count + packet_samples_count*packet_mean) / (total_samples_count + packet_samples_count);
                 total_samples_count += packet_samples_count;
             }
